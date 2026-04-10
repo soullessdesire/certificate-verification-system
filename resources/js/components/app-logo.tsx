c67e4371724253829dfd2246ab3087553e6771de
soul32 <soullessdesire34@gmail.com>
2026-04-10 06:02:15 +0300

    Improved Configureation

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,14 +1,14 @@
-import AppLogoIcon from '@/components/app-logo-icon';
+import { ShieldCheck } from "lucide-react";
 
 export default function AppLogo() {
     return (
         <>
             <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
-                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
+               <ShieldCheck size={40} className="text-primary"/>
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
                 <span className="mb-0.5 truncate leading-tight font-semibold">
-                    Laravel Starter Kit
+                    Certificate Verification System
                 </span>
             </div>
         </>
